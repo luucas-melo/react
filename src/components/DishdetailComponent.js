ba628a7f3bc5257243436dd8536e4ac1332d11c8
luucasxd <lucasmelodos322@gmail.com>
2020-07-02 18:25:35 -0300

    arrumando o nome da classe

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-class DishDetail extends Component {
+class Dishdetail extends Component {
     constructor(props) {
         super(props);
     }
@@ -82,4 +82,4 @@ class DishDetail extends Component {
     }
     
 }
-export default DishDetail;
+export default Dishdetail;
