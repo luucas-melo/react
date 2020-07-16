bf383ae13d4bef9262ea369ac0a83fc3d9dfd94a
luucasxd <lucasmelodos322@gmail.com>
2020-07-16 17:26:59 -0300

    redux actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,8 +27,8 @@ class CommentForm extends Component {
 
 
     handleSubmit(values) {
-        console.log("Current State is:" + JSON.stringify(values))
-        alert("Current State is:" + JSON.stringify(values))
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
     }
     render() {
         return (
@@ -112,7 +112,7 @@ function RenderDish({ dish }) {
     );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
     if (comments != null) {
         const commentList = comments.map((comment) => {
             return (
@@ -135,9 +135,11 @@ function RenderComments({ comments }) {
             <div className="col-12 col-md-5 m-1">
                 <h4>Comments</h4>
                 <ul className="list-unstyled">
+                    <li>
                     {commentList}
-                    <CommentForm />
+                    </li>
                 </ul>
+                    <CommentForm dishId={dishId} addComment={addComment} />
             </div>
         );
     }
@@ -171,7 +173,9 @@ const Dishdetail = (props) => {
                 </div>
                 <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.comments} />
+                    <RenderComments comments={props.comments} 
+                        addComment={props.addComment}
+                        dishId={props.dish.id} />
 
                 </div>
             </div>
